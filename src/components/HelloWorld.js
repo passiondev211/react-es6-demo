73bfe79306ece28ba8694be5d110094e333dc89b
passiondev211 <passiondev@tutanota.com>
2017-11-12 22:12:49 -0300

    test

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,6 +3,6 @@
 import React from 'react';
 import './hello-world.css';
 
-const HelloWorld = () => <h1>Hello, World!</h1>;
+const HelloWorld = () => <h1>Hello, World!!</h1>;
 
 export default HelloWorld;
